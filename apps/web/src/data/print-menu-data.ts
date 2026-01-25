4a069509ee2e63b7aadc3d264aca14b02ad496d6
Thao Pham <43542588+cauvang@users.noreply.github.com>
2026-01-26 10:02:30 +1100

    Rearrange menu items in print-menu-data.ts

--- apps/web/src/data/print-menu-data.ts
+++ apps/web/src/data/print-menu-data.ts
@@ -137,8 +137,8 @@ export const inStoreCategoryLayouts = {
     right: ['FOR VEGETARIANS', 'SNACK PACK', 'SOUVLAKI', 'STEAK SANDWICHES']
   },
   menu2: {
-    left: ['FISH', 'CHIPS', 'CHIPS & GRAVY'],
-    middleCollumn: ['PACKS', 'SPECIAL COMBO'],
+    middleCollumn: ['FISH', 'CHIPS', 'CHIPS & GRAVY'],
+    left: ['PACKS', 'SPECIAL COMBO'],
     right: ['SIDES', 'SEAFOOD SIDES', 'SWEET']
   },
   menu3: {
